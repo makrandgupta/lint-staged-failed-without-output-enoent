d9dd0cd45c72f130314dc2f74ccb40a61f88e158
Makrand <mail@makrand.io>
2021-12-08 01:39:23 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('some test thing')
+var favoriteFruits = ['apple', 'strawberry', 'orange']
+function addFruit(fruit) {
+  favoriteFruits.push(fruit)
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('some test thing')
+
+const favoriteFruits: string[] = ['apple', 'strawberry', 'orange']
+
+function addFruit(fruit: string) {
+  favoriteFruits.push(fruit)
+}
